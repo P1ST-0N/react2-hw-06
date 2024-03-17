c7855ab5cf751b15f66b2ef508cb27c5e19e7680
P1ST-0N <luckylionia@gmail.com>
2024-03-17 16:45:04 +0200

    added SearchBox

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,13 +2,14 @@ import "../../../node_modules/modern-normalize/modern-normalize.css";
 import css from "./App.module.css";
 
 import ContactForm from "../ContactForm/ContactForm";
+import SearchBox from "../SearchBox/SearchBox";
 
 function App() {
   return (
     <div className={css.container}>
       <h1 className={css.mainTitle}>Phonebook 📱</h1>
       <ContactForm />
-      {/* <SearchBox /> */}
+      <SearchBox />
       {/* <ContactList /> */}
     </div>
   );
